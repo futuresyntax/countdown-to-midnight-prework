b59d17f42bbb8a43272865b0c4a50cbb911d6cd4
FutureScript <fernandobaxter1@gmail.com>
2018-11-12 13:36:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,19 +6,10 @@ def countdown(seconds)
   end
   return "HAPPY NEW YEAR!"
 end
-#
-# def countdown_with_sleep(seconds)
-#   while seconds > 0
-#     puts "#{seconds} SECOND(S)!"
-#     seconds -= 1
-#     sleep(1)
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
 
 def countdown_with_sleep(seconds)
   while seconds > 0
-    puts "#{seconds} SECONDS!"
+    puts "#{seconds} SECONDS(S)!"
     seconds -= 1
     sleep(1)
   end
